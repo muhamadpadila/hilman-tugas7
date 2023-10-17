68aa2b91afad99d2a5436b54ffc2d3a46bbe1b60
muhamadpadila <144421993+muhamadpadila@users.noreply.github.com>
2023-10-17 17:04:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const mhs_hasan = {
+    "nama" : "Muhammad hasan Padilah",
+    "umur" : 18,
+    "asal" : "jawa barat",
+    "fakultas" : "teknik informatika"
+}
+
+const mhs_Rasya = {
+    "nama" : "Rasya Saputra",
+    "umur" : 23,
+    "asal" : "jakarta",
+    "fakultas" : "pertanian"
+}
+
+const kelas_petik = [mhs_hasan, ,mhs_Rasya]
+
+for (mhs of kelas_petik){
+    console.log(mhs);
+}
